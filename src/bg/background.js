20e10dc9fe80d586169ad65565e219d6d08cbeb0
micah rich <micah@micahrich.com>
2015-05-12 16:48:26 -0700

    only shows GA icon on trello pages with WDI & calendar in the url

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,20 +1,15 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-
-function showPageAction( tabId, changeInfo, tab ) {
-    chrome.pageAction.show(tabId);
+function showGAIcon(details) {
+    if (details.frameId === 0) {
+        // Top-level frame
+        chrome.pageAction.show(details.tabId);
+    }
+}
+var onlyOnCertainPages = {
+    url: [{
+        hostEquals: 'trello.com',
+        pathContains: 'wdi',
+        pathContains: 'calendar'
+    }]
 };
-// Call the above function when the url of a tab changes.
-chrome.tabs.onUpdated.addListener(showPageAction);
+chrome.webNavigation.onCommitted.addListener(showGAIcon, onlyOnCertainPages);
+chrome.webNavigation.onHistoryStateUpdated.addListener(showGAIcon, onlyOnCertainPages);
